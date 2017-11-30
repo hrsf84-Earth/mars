fe1a1ccfcd806be13e076009b7a2eb41f27a4cb1
Janet Lee <5065781+janetlee@users.noreply.github.com>
2017-11-29 16:23:39 -0800

    Create initial twitterLocation.js and locationTweets.js spec files (#2)

---
+++ specs/locationTweets.js
@@ -0,0 +1,8 @@
+// locationTweets.js
+// These are the plain English language tests I would like to perform.
+
+// The widget should be a statless component and get the query term & lat/long states from the store.
+// The widget should accept the lat/long format as input and render data.
+// The widget should accept a query search term as input and render data.
+// The widget should render a graph of data.
+// The widget should paint sentiment information.
---
+++ utils/twitterLocation.js
@@ -0,0 +1,77 @@
+const axios = require('axios');
+
+// Magically import/borrow auth credentials and
+// the getEmotions function from the other location
+
+// What is going to give me the lat/long after it is selected
+ x
+// const twitterApiUrl = 'https://api.twitter.com/1.1/search/tweets.json';
+// const twitterToken = process.env.TWITTERAPI;
+
+// const watsonApiUrl = 'https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze';
+// const watsonToken = process.env.WATSONAPI;
+
+// const getEmotions = function (text) {
+//   return axios.get(
+//     watsonApiUrl,
+//     {
+//       params: {
+//         version: '2017-02-27',
+//         features: 'emotion',
+//         text,
+//       },
+//       headers: {
+//         Authorization: `Basic ${watsonToken}`,
+//       },
+//     },
+//   )
+//     .catch((err) => {
+//       console.log(`error from watson API:  ${err}`);
+//     }).then(Promise.resolve(false));
+// };
+
+//  returns an object with emotion properties
+// I think this needs default lat/long if nothing is provided, then the shape would be for the entire US.
+module.exports.avgLocationEmotion = function (twitterSearchTerm, lat, long) {
+  let geocodeString = lat + ',' + long + ',' + '50mi'
+  return axios.get(
+    twitterApiUrl,
+    {
+      params: {
+        q: twitterSearchTerm,
+        geocode: geocodeString
+      },
+      headers: {
+        Authorization: `Bearer ${twitterToken}`,
+      },
+    },
+  )
+    .then(res => res.data.statuses.map(status => status.text))
+    .then((texts) => {
+      const emotions = [];
+      texts.forEach(text => emotions.push(getEmotions(text)));
+      return Promise.all(emotions);
+    })
+    .then((emotions) => {
+      const avgEmotion = {
+        sadness: 0, joy: 0, fear: 0, disgust: 0, anger: 0,
+      };
+      const props = Object.keys(avgEmotion);
+      let emotionCount = 0;
+      emotions.forEach((emotion) => {
+        if (emotion) {
+          for (let i = 0; i < props.length; i += 1) {
+            avgEmotion[props[i]] += emotion.data.emotion.document.emotion[props[i]];
+          }
+          emotionCount += 1;
+        }
+      });
+      for (let i = 0; i < props.length; i += 1) {
+        avgEmotion[props[i]] /= emotionCount;
+      }
+      return avgEmotion;
+    })
+    .catch((err) => {
+      console.log(`error from twitter api:  ${err}`);
+    });
+};
