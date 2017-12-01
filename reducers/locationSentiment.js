6431612e3d464d8365c67a1400d7cb2bd47386c7
JeffreyStocker <31262527+JeffreyStocker@users.noreply.github.com>
2017-12-01 13:37:44 -0800

    Removed extra character

--- reducers/locationSentiment.js
+++ reducers/locationSentiment.js
@@ -4,7 +4,6 @@ export default function (state = {latitude: 40.7127753, longitude:-74.0059728}, 
   switch (action.type) {
     case FETCH_SENTIMENT:
       return action.payload.data;
-      ;
     default:
       return state;
   }
