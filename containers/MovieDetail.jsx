eb96591d869fc314c20546e99ad400568c8f1b48
Jeff <jeffreyRstocker@gmail.com>
2017-12-01 09:01:46 -0800

    Implemented a navication buttons to switch between graph or map views

--- containers/MovieDetail.jsx
+++ containers/MovieDetail.jsx
@@ -5,20 +5,46 @@ import Paper from 'material-ui/Paper';
 import Title from '../components/Title';
 import Graph from './Graph';
 import MovieInfo from '../components/MovieInfo';
+import Navigation from '../components/Navigation';
 
 const style = { padding: '35px' };
 
+import Store from '../public/index.jsx'
+
 function MovieDetail(props) {
+  var { mainView } = props.mainView;
+  var returnDom;
+  console.log('props', mainView)
+  if (mainView === 'graphAbsolute') {
+    var returnDom =
+      <Graph
+        primaryMovie={props.primaryMovie}
+        secondaryMovie={props.secondaryMovie}
+      />
+  } else if (mainView === 'graphRelative') {
+    var returnDom =
+      <Graph
+        primaryMovie={props.primaryMovie}
+        secondaryMovie={props.secondaryMovie}
+      />
+  } else if (mainView === 'map') {
+    var returnDom = null;
+  } else {
+    var returnDom = <Graph
+    primaryMovie={props.primaryMovie}
+    secondaryMovie={props.secondaryMovie}
+  />
+  }
+
   return (
     <Paper zDepth={1} style={style}>
       <Title
         primaryMovie={props.primaryMovie}
         secondaryMovie={props.secondaryMovie}
       />
-      <Graph
-        primaryMovie={props.primaryMovie}
-        secondaryMovie={props.secondaryMovie}
+      <Navigation
       />
+      {returnDom}
       <MovieInfo
         primaryMovie={props.primaryMovie}
         secondaryMovie={props.secondaryMovie}
@@ -34,8 +60,8 @@ MovieDetail.propTypes = {
   longitude: PropTypes.number
 };
 
-function mapStateToProps({ primaryMovie, secondaryMovie, latitude, longitude }) {
-  return { primaryMovie, secondaryMovie, latitude, longitude };
+function mapStateToProps({ primaryMovie, secondaryMovie, latitude, longitude, mainView }) {
+  return { primaryMovie, secondaryMovie, latitude, longitude, mainView };
 }
 
 export default connect(mapStateToProps)(MovieDetail);
