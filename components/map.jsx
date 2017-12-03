51bba9c7691a247c5d2574f18a99584f3791cdb5
Ted Anyansi <tanyansi@gmail.com>
2017-12-02 17:19:33 -0800

    made some changes for enhanced code clarity

--- components/map.jsx
+++ components/map.jsx
@@ -6,24 +6,17 @@ class Map extends React.Component {
   constructor(props) {
     super(props);
     this.onSearch = this.onSearch.bind(this);
+
   }
 
   componentDidMount() {
-    const theMap = this.refs.map;
-    const mapOptions = {
-      center: {lat: 37.773972,
-      lng: -122.431297},
-      zoom: 12,
-      minZoom: 3
-    };
-
     this.initializeMap();
   }
 
   initializeMap() {
     var chicago = {lat: 41.850033, lng: -87.6500523};
 
-    var INITIAL_LOCATION = {
+    var sanFrancisco = {
       address: 'San Francisco, CA',
       position: {
         lat: 37.773972,
@@ -37,11 +30,10 @@ class Map extends React.Component {
     });
 
     this.marker = new google.maps.Marker({
-
       map: this.map,
       position: {
-        lat: this.props.lat || INITIAL_LOCATION.position.lat,
-        lng: this.props.lng || INITIAL_LOCATION.position.lng
+        lat: this.props.lat || sanFrancisco.position.lat,
+        lng: this.props.lng || sanFrancisco.position.lng
       }
     });
 
@@ -56,21 +48,23 @@ class Map extends React.Component {
       if(status === google.maps.GeocoderStatus.OK) {
         context.map.setCenter(results[0].geometry.location);
         context.marker.setPosition(results[0].geometry.location);
-
         return;
-
       }
     })
   }
 
-
   render() {
-    return (<div>
+    var mapStyle = {
+      marginBottom: '5px', backgroundColor: 'red', width: '100%', height: '390px', borderRadius: '5px'
+    }
+    return (
+    <div>
       <br />
       <Mapsearch onMapSearch={this.onSearch}/>
 
-      <div className="map" ref="map" style={{marginBottom: '5px', backgroundColor: 'red', width: '100%', height: '390px', borderRadius: '5px'}}> </div>
-    </div>)
+      <div className="map" ref="map" style={mapStyle}> </div>
+    </div>
+  )
   }
 }
 
